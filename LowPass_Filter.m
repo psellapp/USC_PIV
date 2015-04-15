213a547cc63ea14f421bdfd30295ed52b580666b
Prabu Sellappan <psellapp@users.noreply.github.com>
2015-04-14 20:12:59 -0700

    Low-pass Butterworth filter

---
+++ LowPass_Filter.m
@@ -0,0 +1,24 @@
+function [filt_signal] = LowPass_Filter(orig_signal,filter_order,F_s,cutoff_freq)
+% Filter a signal using low pass Butterworth filter and output filtered signal.
+% usage: [filt_signal] = LowPass_Filter(orig_signal,filter_order,F_s,cutoff_freq)
+%
+% created: Prabu, 4/14/2015
+%
+% filt_signal - filtered signal with frequencies > cutoff attenuated; 1-D
+%               vector of length N
+% orig_signal - original signal, 1-D vector of length N
+% filter_order - order of filter; higher orders have steeper roll-off
+% F_s - Sampling rate, in Hz
+% cutoff_freq - cut-off frequency, in Hz
+%
+
+if cutoff_freq<(F_s/2) %Ensure cut-off freq is less than Nyquist frequency
+    [num,den]=butter(filter_order,cutoff_freq/(F_s/2)); %calculate Butterworth filter coefficients
+    filt_signal=filter(num,den,orig_signal); %apply filter to signal
+else
+    disp('cut-off frequency must be less than half the sampling rate');
+    filt_signal = [];
+end
+
+end
+
