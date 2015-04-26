fcd81d16194be3851c0509f30a477f850d8953fb
Prabu Sellappan <psellapp@users.noreply.github.com>
2015-04-25 17:25:49 -0700

    High-pass Butterworth filter -modified to use filtfilt function to ensure zero phase distortion

--- HighPass_Filter.m
+++ HighPass_Filter.m
@@ -1,8 +1,13 @@
 function [filt_signal] = HighPass_Filter(orig_signal,filter_order,F_s,cutoff_freq)
-% Filter a signal using high pass Butterworth filter and output filtered signal.
+% Filter a signal using high pass Butterworth filter and output filtered
+% signal. 
+% Note: Filter order is effectively doubled due to the use of zero-phase 
+% distortion filtfilt function.
+% 
 % usage: [filt_signal] = HighPass_Filter(orig_signal,filter_order,F_s,cutoff_freq)
 %
 % created: Prabu, 4/15/2015
+% modified: PS, 4/25/2015. Use filtfilt for zero-phase filtering
 %
 % filt_signal - filtered signal with frequencies < cutoff attenuated; 1-D
 %               vector of length N
@@ -13,8 +18,8 @@ function [filt_signal] = HighPass_Filter(orig_signal,filter_order,F_s,cutoff_fre
 %
 
 if cutoff_freq<(F_s/2) %Ensure cut-off freq is less than Nyquist frequency
-    [num,den]=butter(filter_order,cutoff_freq/(F_s/2),'high'); %calculate Butterworth filter coefficients
-    filt_signal=filter(num,den,orig_signal); %apply filter to signal
+    [num,den]=butter(filter_order,cutoff_freq/(F_s/2),'high'); %calculate Butterworth filter coefficients;
+    filt_signal=filtfilt(num,den,orig_signal); %apply filter to signal
 else
     disp('cut-off frequency must be less than half the sampling rate');
     filt_signal = [];
